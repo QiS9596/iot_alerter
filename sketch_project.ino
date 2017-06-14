cf0f3fc44ab0cfc992d37e9cf835779ef0489c60
Rolo <rolo_lamperouge@foxmail.com>
2017-06-14 11:57:10 +0800

    modified handleMsg method

--- sketch_project.ino
+++ sketch_project.ino
@@ -3,29 +3,57 @@
 const int pinLed = 13;
 char phone_number_list[5][20];
 int top_phone = 0;
+String str_buffer;
+void sendMsg(char p_num[], String msg);
+bool test = 1;
+int time = 1000;
 void handleMsg(char dtaget[], char p_num[]){
-  if(strcmp(dtaget,"set num")){
-      strncpy(phone_number_list[top_phone],p_num,20);
-      top_phone ++;
-    }
 
+  char * piece;
+  piece = strtok(dtaget,"\n");
+  if(!strcmp(piece,"[setting]")){
+    for(int i = 0; i < 3; i++){
+      piece = strtok(NULL,"\n");
+      if(i<=1)
+        strcpy(phone_number_list[i],piece);
+      else time = atoi(piece);
+    }  
+  }
+}
+
+void sendMsg(char p_num[], String msg){
+  LSMS.beginSMS(p_num);
+  LSMS.print(msg);
+  if(LSMS.endSMS()){
+    Serial.println("SMS sent");  
+  }else{
+    Serial.println("SMS is not sent");
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  /*
   while(!LSMS.ready()){
     delay(100);  
-  }
+  }*/
   Serial.println("GSM OK!!");
+  
 }
-
+void printSettingInfo(){
+  Serial.println(phone_number_list[0]);
+  Serial.println(phone_number_list[1]);
+}
 void loop() {
   // put your main code here, to run repeatedly:
   char p_num[20];
   int len = 0;
   char dtaget[500];
 //  Serial.println(LSMS.available());
+  strcpy(dtaget , "[setting]\n0981936325\n0811224156\n15");
+  handleMsg(dtaget,"11111111");
+  printSettingInfo();
   if(LSMS.available()){
     LSMS.remoteNumber(p_num,20);
     Serial.println("There is new message.");
